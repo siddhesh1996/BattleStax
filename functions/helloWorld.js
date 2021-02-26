f90702ff8a3788418ee95c94794d237282ab28a3
Siddhesh Kasar <siddhesh@sportzinteractive.net>
2021-02-26 14:16:47 +0530

    First serveless function

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,3 +1,7 @@
 exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: "world" })
+  }
   // let's return a JSON response that looks like: { hello: "world" }
 };
